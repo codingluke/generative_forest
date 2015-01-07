28ae58bc43e72bbc8914a45ca1139e3c0e38bc8a
Richard Remus <s0542820@htw-berlin.de>
2015-01-07 12:59:31 +0100

    Added two mesh-files.

---
+++ src/CMakeFiles/Forest.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Forest.dir/main.cpp.o"
+  "../Binaries/Darwin/Release/Forest.pdb"
+  "../Binaries/Darwin/Release/Forest.app/Contents/MacOS/Forest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Forest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
